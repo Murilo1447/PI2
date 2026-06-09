24dd9a956d669085f8305effe2b87d051fe7a613
MURILO SERGIO DE SANTANA MACIEL <murilo.ssmaciel@senacsp.edu.br>
2026-06-09 16:26:56 -0300

    atualização

--- Gen_Taref/__init__.py
+++
@@ -1,8 +0,0 @@
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-
-app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///Gen_Taref.db'
-database = SQLAlchemy(app)
-
-from Gen_Taref import routes, models
--- main.py
+++
@@ -1,5 +0,0 @@
-from Gen_Taref import app
-
-
-if __name__ == '__main__':
-    app.run(debug=True)
